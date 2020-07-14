bbba1bcd6a5b39efcb42508c2f9268900ca44261
patricia <patricia.selpa@gmail.com>
2020-07-13 22:13:46 -0300

    Excercices class day 13/07

--- Program.cs
+++ Program.cs
@@ -4,27 +4,160 @@ namespace primeira_aula
 {
     class Program
     {
-        static void Main(string[] args)
+        static void NumerosCrescentes(string[] args)
         {
-            
-            Console.WriteLine("Qual é sua idade?");
-            //Tranformando o texto que o usuário digitou para integer
-            var userAge = Int32.Parse(Console.ReadLine());
-            
-            //Comparando se o valor da variável amILegalAge é true
-            if(userAge > 15 && userAge < 51){
-                Console.WriteLine("Sim, você tem idade para o entra21");
+            var count = 1;
+
+            while (count >= 10){
+                System.Console.WriteLine(count);
+                count++;
             }
-            // else if(userAge == 14 || userAge == 15){
-            //     //Se não, o valor da variável amILegalAge é false
-            //     Console.WriteLine("Não, você não tem idade para o entra21, mas tem para o menor aprendiz");
-            // }
-            else
-            {
-                Console.WriteLine("Não tem idade para nenhum dos dois!");
-            }
-          
+                   
                  
          }
+
+         static void NumerosDecrescentes(string[] args)
+        {
+            var count = 10;
+
+            while (count > 0){
+                System.Console.WriteLine(count);
+                count--;
+            }
+                   
+                 
+         }
+
+         static void NumerosCrescentesPares(string[] args)
+        {
+            var count = 1;
+
+            while (count <= 10){
+                if(count % 2 == 0){
+                    System.Console.WriteLine(count);
+                }
+                count++;
+            }
+                   
+                 
+         }
+        static void SomaDe1a100(string[] args)
+        {
+            var count = 1;
+            int n = 0;
+
+            while (count <= 100){
+                n +=count;
+                System.Console.WriteLine(n);
+                count++;
+            }
+            
+                 
+         }
+
+        static void ImparesMenosresQue200(string[] args)
+        {
+            int count = 1;
+            while (count < 200){
+                if(count % 2 != 0){
+                    System.Console.WriteLine(count);
+                }
+                count++;
+            }
+            
+                 
+         }
+         
+         static void MediaIdade(string[] args)
+        {
+            double sum = 0.0;
+            double count = 0.0;
+
+            while (true)
+            {
+                System.Console.WriteLine("Digite uma idade");
+                var idade = Int32.Parse(System.Console.ReadLine());
+
+                if(idade == 0){
+                    break;
+                }
+                sum += idade;
+                count++;
+            }
+
+            System.Console.WriteLine($"A média de idades da turma é" + String.Format("(0:0.00)", sum/count));
+
+                 
+         }
+
+          static void IdadesPercentagemMulheres(string[] args)
+        {
+            
+            var age = 0.0;
+            var womenNames = "";
+            var count = 0.0;
+
+            while (count < 5)
+            {
+                System.Console.WriteLine("Digit woman's name: ");
+                womenNames = System.Console.ReadLine();
+                System.Console.WriteLine("Digit woman's age: ");
+                int n = Int32.Parse(System.Console.ReadLine());
+                
+                if(n >= 18 && n <= 35 ){
+                    age++;    
+                }
+                count++;
+
+            }
+            System.Console.WriteLine($"A porcentagem de mulher com idades entre 18 e 35 é: {(age/count)*100} ");
+            
+                 
+         }
+
+           static void Main(string[] args)
+        {
+            var candidates = new string[2];   
+            var option = 0;
+            var candidate1Vote = 0;
+            var candidate2Vote = 0;
+            var counterInputCandidates = 0;
+            
+            while(true){
+                System.Console.WriteLine("Selecione o modo de operação da urna:");
+                System.Console.WriteLine(" 1) Cadastrar candidato/ 2) Votação / 3) Apuração de votos");
+                option = Int32.Parse(System.Console.ReadLine());
+                if(option == 1){
+                    System.Console.WriteLine("Por favor, insira a senha: ");
+                    var password = System.Console.ReadLine(); 
+                    if(password == "Pa$$w0rd"){
+                        while(counterInputCandidates < 2){
+                            var intermediateCandidates = System.Console.ReadLine();
+                            candidates[counterInputCandidates] = intermediateCandidates;
+                            counterInputCandidates++;
+                        }
+                    }else{
+                        System.Console.WriteLine("Senha errada!!");
+                        continue;
+                    }
+                }else if(option == 2){
+                    if(candidates.Length == 0){
+                        System.Console.WriteLine("Por favor, insira os candidados primeiro");
+                        continue;
+                    }
+                    var optionVoteOrExit = 0;
+                    System.Console.WriteLine($"Aperte 1 para o candidato {candidates[0]} ou 2 para o {candidates[1]}. Pressione 0 para encerrar a votação");
+
+                    while(optionVoteOrExit != 0){
+                        //ToDoAtHome  
+                        if(optionVoteOrExit == 1){
+
+                        }  
+                    }
+                    }
+                }
+
+            }
+        }
      }
  }
